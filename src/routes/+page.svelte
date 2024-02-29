77acb02e571a2796f32c1f478a924f95cc65c324
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2024-02-29 14:12:25 -0800

    feat: add first component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  import Console from '../lib/pH-console.svelte'
+</script>
+
+<h1>Welcome to pH simmulator</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Console />
