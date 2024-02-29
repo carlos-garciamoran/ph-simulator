85bedb77d532e57e4d32b65885be4d45d88a19d3
imamovick <lc21-1046@lclark.edu>
2024-02-29 15:30:43 -0800

    pH console

--- src/lib/pH-console.svelte
+++ src/lib/pH-console.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from "./components/ui/button/button.svelte";
+	import Checkbox from "./components/ui/checkbox/checkbox.svelte";
 
 	let count = 0;
 
@@ -8,17 +9,21 @@
 	}
 </script>
 
-<div class="border-2 w-full">
-	<p>pH</p>
-	<div class="flex gap-2 mx-auto w-full">
-		<button on:click={handleClick}>
-			Insert probe
-		</button>
-		<button class="border rounded-md" on:click={handleClick}>
-			Remove probe
-		</button>
+<div class="border-2 max-w-sm mx-auto p-4 m-4">
+	<div class="flex justify-between items-center mt-1 ml-4 mr-4 mb-2">
+		<p>pH</p>
+		<span id="phValue">7.0</span>
+	</div>
+		<div class="flex items-center justify-center mb-2">
+			<Checkbox class="mr-2"></Checkbox> 
+			<p>pH Indicator Solution</p>
+		</div>
+	<div class="flex gap-2 mx-auto w-full justify-center mb-2">
 		<Button on:click={handleClick}>
-			Insert probe
+			Insert Probe
+		</Button>
+		<Button on:click={handleClick}>
+			Remove probe
 		</Button>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import Console from '../lib/pH-console.svelte'
 </script>
 
-<div class="border p-8 m-8 ">
-  <h1 class="text-xl">Welcome to pH simmulator</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <Console />
+<div class="flex">
+  <div class="border p-8 m-8 ">
+    <h1 class="text-xl">Welcome to pH simmulator</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <Console />
+  </div>
 </div>
