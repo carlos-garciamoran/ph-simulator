ef624f7db4b7aa07c67be8a047741924a7f91407
lc20-0103 <lc20-0103@lclark.edu>
2024-03-05 13:50:01 -0800

    latest calc and const .ts's

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,57 +19,57 @@ export const pKa_carbonic_acid = 6.367  // Used for H2CO3
 export const initial_water_pH = 7
 export const drop_volume = 0.036
 export const drops=0;
-export const base_conc=0
-export const acid_conc=0
-export const M_HCl=0
-export const M_NaOH=0;
-export const Hplus=0;
-export const excessHplus=0;
-export const excessOH=0;
-export const OH=0;
+// export const base_conc=0
+// export const acid_conc=0
+// export const M_HCl=0
+// export const M_NaOH=0;
+// export const Hplus=0;
+// export const excessHplus=0;
+// export const excessOH=0;
+// export const OH=0;
 
 // chemical concentrations, safe measure in case multiple chemicals require accessing the same conc variable
 
-export const Ba_OH_2_conc=0;
+// export const Ba_OH_2_conc=0;
 
-// Calcium Hydroxide, strong base
-export const CA_OH_2_conc=0;
+// // Calcium Hydroxide, strong base
+// export const CA_OH_2_conc=0;
 
-// Sodium Hydroxide, strong base
-export const NaOH_conc=0;
+// // Sodium Hydroxide, strong base
+// export const NaOH_conc=0;
 
-// Ammonium Hydroxide, weak base
-export const NH4OH_conc=0;
+// // Ammonium Hydroxide, weak base
+// export const NH4OH_conc=0;
 
-// Hydrochloric Acid, strong acid
-export const HCl_conc=0;
+// // Hydrochloric Acid, strong acid
+// export const HCl_conc=0;
 
-// Nitric Acid, strong acid
-export const HNO3_conc=0;
+// // Nitric Acid, strong acid
+// export const HNO3_conc=0;
 
-// Acetic Acid, weak acid
-export const HC2H3O2_conc=0;
+// // Acetic Acid, weak acid
+// export const HC2H3O2_conc=0;
 
-// Carbonic Acid, weak acid
-export const H2CO3_conc=0;
+// // Carbonic Acid, weak acid
+// export const H2CO3_conc=0;
 
-// Sodium Chloride, neutral salt
-export const NaCl=7;
+// // Sodium Chloride, neutral salt
+// export const NaCl=7;
 
-// Ammonium Chloride, acidic salt of NH3
-export const NH4Cl_conc=0;
+// // Ammonium Chloride, acidic salt of NH3
+// export const NH4Cl_conc=0;
 
-// Sodium Acetate, basic salt of HC2H3O2
-export const NaC2H3O2_conc=0;
+// // Sodium Acetate, basic salt of HC2H3O2
+// export const NaC2H3O2_conc=0;
 
-// Sodium Bicarbonate, basic salt H2CO3
-export const NaHCO3_conc=0;
+// // Sodium Bicarbonate, basic salt H2CO3
+// export const NaHCO3_conc=0;
 
-// Sodium Carbonate, basic salt of HCO3
-export const Na2CO3_conc=0;
+// // Sodium Carbonate, basic salt of HCO3
+// export const Na2CO3_conc=0;
 
-// Sodium Bisulfate, acidic salt of H2SO4
-export const NaHSO4_conc=0;
+// // Sodium Bisulfate, acidic salt of H2SO4
+// export const NaHSO4_conc=0;
 
 // HOUSEHOLD ITEMS
 
