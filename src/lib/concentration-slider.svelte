d46534ecf7cad901a1607c2f0aed38a819ff4885
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2024-03-07 13:44:41 -0800

    feat: add concentration slider, ui improvements

--- src/lib/concentration-slider.svelte
+++ src/lib/concentration-slider.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import { Slider } from "$lib/components/ui/slider";
+
+  let concentration = [0.7];
 </script>
- 
-<Slider value={[50]} max={100} step={1} class="max-w-[70%]" />
+
+<!-- TODO: add settings -->
+<div class="border p-6 flex flex-col gap-2">
+  <h2>Concentration (molarity)</h2>
+  <Slider value={concentration} min={0.0001} max={0.1} step={0.0001} class="w-full" />
+  <p class="text-center">{concentration} M</p>
+</div>
--- src/lib/pH-console.svelte
+++ src/lib/pH-console.svelte
@@ -1,29 +1,38 @@
 <script>
 	import Button from "./components/ui/button/button.svelte";
 	import Checkbox from "./components/ui/checkbox/checkbox.svelte";
+	import ArrowUp from "./icons/arrow-up.svelte";
+	import ArrowDown from "./icons/arrow-down.svelte";
+	import Label from "./components/ui/label/label.svelte";
 
-	let count = 0;
+	let checked = false;
 
-	function handleClick() {
-		count += 1;
+	// TODO: implement
+	function handleRemoveProbe() {
+		console.log("Probe removed");
+	}
+	function handleInsertProbe() {
+		console.log("Probe removed");
 	}
 </script>
 
-<div class="border-2 max-w-sm mx-auto p-4 m-4 h-fit">
-	<div class="flex justify-between items-center mt-1 ml-4 mr-4 mb-2">
-		<p>pH</p>
-		<span id="phValue">7.0</span>
+<div class="border w-full mx-auto p-6 h-fit flex flex-col gap-3">
+	<div class="flex justify-between items-center">
+		<p>Current pH</p>
+		<span id="phValue" class="bg-secondary rounded-md px-3 py-2 font-semibold">7.0</span>
 	</div>
-		<div class="flex items-center justify-center mb-2">
-			<Checkbox class="mr-2"></Checkbox> 
-			<p>pH Indicator Solution</p>
-		</div>
-	<div class="flex gap-2 mx-auto w-full justify-center mb-2">
-		<Button on:click={handleClick}>
-			Insert Probe
-		</Button>
-		<Button on:click={handleClick}>
+	<div class="flex items-center gap-2">
+		<Checkbox id="indicator-solution" bind:checked />
+		<Label for="indicator-solution">pH Indicator Solution</Label>
+	</div>
+	<div class="flex gap-2 mx-auto flex-col w-full justify-center mt-4">
+		<Button class="gap-2" on:click={handleRemoveProbe}>
 			Remove probe
+			<svelte:component this={ArrowUp} />
+		</Button>
+		<Button class="gap-2" on:click={handleInsertProbe}>
+			Insert Probe
+			<svelte:component this={ArrowDown} />
 		</Button>
 	</div>
 </div>
