9ccee561b02eb72a540c69ea62c3ae19937c7f08
Aleksa <aleksa.belic@gmail.com>
2024-03-19 15:19:16 -0700

    commented out dropper and probe to fix bug with

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
   import Console from '$lib/pH-console.svelte'
   import Menus from '$lib/menus.svelte'
   import Beaker from '$lib/beaker.svelte'
-	import Probe from '@/probe.svelte';
-  import Dropper from '@/dropper.svelte';
+	// import Probe from '@/probe.svelte';
+  // import Dropper from '@/dropper.svelte';
 </script>
 
 <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 size-full p-6 lg:p-10">
   <Menus />
   <div class="">
       <Beaker />
-      <Dropper />
-      <Probe />
+      <!-- <Dropper />
+      <Probe /> -->
   </div>
   <Console />
 </div>
