0f6797b36301a3b512bb3516992969375c831910
Aleksa <aleksa.belic@gmail.com>
2024-03-21 13:55:29 -0700

    slider pseudo

--- src/lib/concentration-slider.svelte
+++ src/lib/concentration-slider.svelte
@@ -18,11 +18,22 @@
 <!-- PSEUDO CODE
 if acids
   acids slider
+    slider with exact values of: 
+    .1 .09 .08 .07 .06 .05 .04 .03 .02 .01
+    .009 .008 .007 .006 .005 .004 .003 .002 .001
+    .0009 .0008 .0007 .0006 .0005 .0004 .0003 .0002 .0001
 else if salts
   salts slider
+    slider with exact values of:
+      .1 .09 .08 .07 .06 .05 .04 .03 .02 .01
+      .009 .008 .007 .006 .005 .004 .003 .002 .001
 else if buffers
   buffer slider 1
   buffer slider 2
+  sliders exact values
+    1 .9 .8 .7 .6 .5 .4 .3 .2 .1
+  .09 .08 .07 .06 .05 .04 .03 .02 .01
+  .009 .008 .007 .006 .005 .004 .003 .002 .001
   dropper
     Goes from 0 to 20 drops
     Changes volume by .036 mL per drop
