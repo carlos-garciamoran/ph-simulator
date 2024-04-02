ec8fa092b9e855ff2c683eb39553c802cddec75a
Aleksa <aleksa.belic@gmail.com>
2024-04-02 14:55:09 -0700

    placeholder for currentMenu store

--- src/lib/helpers/store.ts
+++ src/lib/helpers/store.ts
@@ -11,3 +11,5 @@ export const phValueStore: Writable<number> = writable();
 
 // 0 means the probe is up, 1 means it's down
 export const probePosition = writable(0);
+
+// export const currentMenu = writable('acids'); // Default to 'acids' or any other default value you prefer
