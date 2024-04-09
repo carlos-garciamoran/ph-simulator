21a925e80e605540eae069f2db521d2594b62624
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2024-04-09 14:46:07 -0700

    chore: add drop count stores

--- src/lib/helpers/store.ts
+++ src/lib/helpers/store.ts
@@ -19,8 +19,9 @@ export const phValueStore: Writable<number> = writable();
 // 0 means the probe is up, 1 means it's down
 export const probePosition = writable(0);
 
-// number of drops used, 20 max
-export const dropCount = writable(0);
+// Number of drops used, 20 max
+export const dropCountHCl = writable(0);
+export const dropCountNaOH = writable(0);
 
 // Current menu selected
 export const menu: Writable<Menu> = writable('acids/bases');
