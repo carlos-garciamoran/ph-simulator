31f76b9603ff1019945b84fdf71057a3be1b928e
Aleksa <aleksa.belic@gmail.com>
2024-04-16 15:19:23 -0700

    probe goes up on 0

--- src/lib/helpers/reset.ts
+++ src/lib/helpers/reset.ts
@@ -1,4 +1,4 @@
-import { bufferConcentration, dropsCounter, sliderIndex, totalDrops, totalVolume } from './store';
+import { bufferConcentration, dropsCounter, probePosition, sliderIndex, totalDrops, totalVolume } from './store';
 
 export function resetValues() {
 	dropsCounter.set({ HCl: 0, NaOH: 0 });
@@ -6,4 +6,5 @@ export function resetValues() {
 	sliderIndex.set(0); // Reset the slider index
 	totalDrops.set(0); // Reset the total number of drops
 	bufferConcentration.set({ acid: 0.001, base: 0.001 }); // Reset the buffer concentrations
+	probePosition.set(0);
 }
